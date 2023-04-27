fb3e21bdd21791284eea90ac3604da7f7a33a7eb
Yagth <yabuman46@gmail.com>
2023-04-27 14:43:09 +0300

    Petya and Strings finished

---
+++ Code-Force/Haskell/Petya and Strings.hs
@@ -0,0 +1,12 @@
+import Data.Char(toLower)
+main :: IO ()
+main = do
+    inputs <- getContents
+    let [s1, s2] = words inputs
+    case compare' s1 s2 of
+        EQ -> print 0
+        LT -> print (-1)
+        GT -> print 1
+
+compare' :: String -> String -> Ordering
+compare' s1 s2 = map toLower s1 `compare` map toLower s2
