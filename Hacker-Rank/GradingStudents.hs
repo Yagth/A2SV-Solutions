d5640a7d9393a3a2b748e50d8c212bd5a0af5fff
Yagth <yabuman46@gmail.com>
2023-04-28 18:52:35 +0300

    Some more problems

---
+++ Hacker-Rank/GradingStudents.hs
@@ -0,0 +1,62 @@
+-- {-# LANGUAGE DuplicateRecordFields, FlexibleInstances, UndecidableInstances #-}
+
+-- module Main where
+
+-- import Control.Monad
+-- import Data.Array
+-- import Data.Bits
+-- import Data.List
+-- -- import Data.List.Split
+-- import Data.Set
+-- import Data.Text
+-- import Debug.Trace
+-- import System.Environment
+-- import System.IO
+-- import System.IO.Unsafe
+
+--
+-- Complete the 'gradingStudents' function below.
+--
+-- The function is expected to return an INTEGER_ARRAY.
+-- The function accepts INTEGER_ARRAY grades as parameter.
+--
+
+gradingStudents =map (\grade -> if failureGrade grade then grade else roundGrade grade 0)
+
+
+failureGrade :: Integer -> Bool
+failureGrade = (< 38)
+
+roundGrade :: Integer -> Integer -> Integer
+roundGrade grade diff
+    | diff > 2 = grade
+    | (grade + diff) `mod` 5 == 0 = grade + diff
+    | otherwise = roundGrade grade (diff + 1)
+
+-- lstrip = Data.Text.unpack . Data.Text.stripStart . Data.Text.pack
+-- rstrip = Data.Text.unpack . Data.Text.stripEnd . Data.Text.pack
+
+-- readMultipleLinesAsStringArray :: Int -> IO [String]
+-- readMultipleLinesAsStringArray 0 = return []
+-- readMultipleLinesAsStringArray n = do
+--     line <- getLine
+--     rest <- readMultipleLinesAsStringArray(n - 1)
+--     return (line : rest)
+
+-- main :: IO()
+-- main = do
+--     stdout <- getEnv "OUTPUT_PATH"
+--     fptr <- openFile stdout WriteMode
+
+--     gradesCountTemp <- getLine
+--     let gradesCount = read $ lstrip $ rstrip gradesCountTemp :: Int
+
+--     gradesTemp <- readMultipleLinesAsStringArray gradesCount
+--     let grades = Data.List.map (read :: String -> Int) gradesTemp
+
+--     let result = gradingStudents grades
+
+--     hPutStrLn fptr $ Data.List.intercalate "\n" $ Data.List.map (\x -> show x) $ result
+
+--     hFlush fptr
+--     hClose fptr
